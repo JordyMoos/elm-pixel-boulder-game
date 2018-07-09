009b49f71f1a6f059e179806274c3368b07d13f4
Jordy Moos <jordymoos@gmail.com>
2018-07-09 23:10:47 +0200

    Fix the camera offset

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -640,18 +640,18 @@ tryMoveCamera level actor camera =
                         transformData.position
 
                     x =
-                        if position.x - camera.borderSize < viewPosition.x then
+                        if position.x - camera.borderSize <= viewPosition.x then
                             position.x - camera.borderSize
-                        else if position.x - view.width + (camera.borderSize * 2) > viewPosition.x then
-                            position.x + (camera.borderSize * 2) - view.width
+                        else if position.x - view.width + camera.borderSize > viewPosition.x - 1 then
+                            position.x - view.width + camera.borderSize + 1
                         else
                             viewPosition.x
 
                     y =
-                        if position.y - camera.borderSize < viewPosition.y then
+                        if position.y - camera.borderSize <= viewPosition.y then
                             position.y - camera.borderSize
-                        else if position.y - view.height + (camera.borderSize * 2) > viewPosition.y then
-                            position.y + (camera.borderSize * 2) - view.height
+                        else if position.y - view.height + camera.borderSize >= viewPosition.y - 1 then
+                            position.y - view.height + camera.borderSize + 1
                         else
                             viewPosition.y
 
