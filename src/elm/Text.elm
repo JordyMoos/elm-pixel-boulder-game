6e06cda0ccd8dada99ad77f9f23bdcde419205ec
Jordy Moos <jordymoos@gmail.com>
2018-07-17 07:49:35 +0200

    Setup the texts so they are actually readable from code

--- src/elm/Text.elm
+++ src/elm/Text.elm
@@ -1,4 +1,4 @@
-module Text exposing (..)
+module Text exposing (Letter, Letters, stringToLetters)
 
 import Color exposing (Color)
 import Dict exposing (Dict)
@@ -16,14 +16,40 @@ type alias Letters =
 
 dictionary : Dict Char Letter
 dictionary =
-    Dict.fromList
-        [ ( 'a', Letter 3 [ ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 0, 4 ), ( 1, 0 ), ( 1, 2 ), ( 2, 1 ), ( 2, 2 ), ( 2, 3 ), ( 2, 4 ) ] )
-        , ( 'b', Letter 3 [ ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 0, 4 ), ( 1, 0 ), ( 1, 2 ), ( 1, 4 ), ( 2, 1 ), ( 2, 2 ), ( 2, 3 ) ] )
-        , ( 'c', Letter 3 [ ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 1, 0 ), ( 1, 4 ), ( 2, 0 ), ( 2, 4 ) ] )
-        , ( 'w', Letter 5 [ ( 0, 0 ), ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 1, 4 ), ( 2, 2 ), ( 2, 3 ), ( 3, 4 ), ( 4, 0 ), ( 4, 1 ), ( 4, 2 ), ( 4, 3 ) ] )
+    Dict.fromList <|
+        [ ( 'a'
+          , [ " o "
+            , "o o"
+            , "ooo"
+            , "o o"
+            , "o o"
+            ]
+          )
+        , ( 'b'
+          , [ "oo "
+            , "o o"
+            , "oo "
+            , "o o"
+            , "oo "
+            ]
+          )
+        , ( 'c'
+          , [ " oo"
+            , "o  "
+            , "o  "
+            , "o  "
+            , " oo"
+            ]
+          )
         ]
+            |> List.map designToLetter
 
 
-stringToPixels : String -> Letters
-stringToPixels string =
+designToLetter : ( Char, List String ) -> Letter
+designToLetter ( char, design ) =
+    Letter char []
+
+
+stringToLetters : String -> Letters
+stringToLetters string =
     []
