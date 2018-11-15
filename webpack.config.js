4b4fd056958ccc053b66af655a2521f9dfc7ff87
Jordy Moos <jordymoos@gmail.com>
2018-11-15 08:52:43 +0100

    Fix the prod build

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var common = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "./",
     // webpack -p automatically adds hash when building for production
     filename: filename
   },
