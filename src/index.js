947f2cc071c21b1799bb63c7483c5070f853f9f0
Jordy Moos <jordymoos@gmail.com>
2018-12-03 22:10:58 +0100

    Working on canvas editor

--- src/index.js
+++ src/index.js
@@ -16,15 +16,52 @@ const levels = {
 };
 
 const sampleLevel = require('./static/levels/test/sample.json');
+const defaultEasyScene = `..o......
+.p...**..
+.........
+.........
+.........
+.........`;
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const startLevel = urlParams.get('startLevel') || null;
 const hideDebug = !! urlParams.get('hideDebug');
-let editorMode = urlParams.get('editorMode') === 'easy' ? 'easy' : 'advanced';
+let editorMode = urlParams.get('editorMode') === 'advanced' ? 'advanced' : 'easy';
 let notEditorMode = getOtherMode(editorMode);
+let subMode = 'image';
+
+function sceneStringToMap(scene) {
+  return scene.split("\n").map(row => row.split(''));
+}
 
 function getOtherMode(mode) {
   return mode === 'easy' ? 'advanced' : 'easy';
+}
+
+function showProperSubMode() {
+  if (subMode === 'image') {
+    document.getElementById('canvas-mode').style.display = '';
+    document.getElementById('ascii-mode').style.display = 'none';
+  } else {
+    document.getElementById('canvas-mode').style.display = 'none';
+    document.getElementById('ascii-mode').style.display = '';
+  }
+}
+
+function combineAsciiScenes(big, other) {
+  let dest = [];
+
+  [big, other].forEach(sceneMap => {
+    sceneMap.forEach((rows, y) => {
+      dest[y] = dest[y] || [];
+      rows.forEach((sign, x) => {
+        dest[y][x] = sign;
+      });
+    });
+  });
+
+  return dest;
 }
 
 ['easy', 'advanced'].forEach(function(mode) {
@@ -44,6 +81,110 @@ document.getElementById(notEditorMode + '-editor').style.display = 'none';
 if (hideDebug) {
   document.getElementById('edit-level-container').style.display = 'none';
 }
+
+let activeSpriteElement = undefined;
+let spriteElements = Array.from(document.getElementById('sprite-container').getElementsByTagName('img'));
+spriteElements.forEach(function (element) {
+  element.addEventListener('click', function (event) {
+    spriteElements.forEach(function (aSpriteElement) {
+      aSpriteElement.className = '';
+    });
+
+    element.className = 'active';
+    activeSpriteElement = element;
+  });
+});
+spriteElements[0].className = 'active';
+activeSpriteElement = spriteElements[0];
+
+let backgroundSpriteElement = document.getElementById('sprite- ');
+let canvasContainerElement = document.getElementById('canvas-editor-container');
+let canvasElement = document.getElementById('canvas-editor-canvas');
+let canvas = canvasElement.getContext('2d');
+let tileSize = 24;
+let canvasWidth = (tileSize + 1) * 50;
+let canvasHeight = (tileSize + 1) * 50;
+canvasElement.width = canvasWidth;
+canvasElement.height = canvasHeight;
+let totalXRows = Math.ceil(canvasWidth / tileSize);
+let totalYRows = Math.ceil(canvasHeight / tileSize);
+
+let emptyAsciiScene = Array(totalYRows).fill(backgroundSpriteElement.dataset.sign)
+  .map(y => Array(totalXRows).fill(backgroundSpriteElement.dataset.sign));
+
+var asciiScene = combineAsciiScenes(
+  emptyAsciiScene,
+  sceneStringToMap(localStorage.getItem(easyCacheKey) || defaultEasyScene)
+);
+
+
+function fullRedraw() {
+  canvas.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  [...Array(totalYRows).keys()].slice(1).forEach(function (yId) {
+    let y = yId * (tileSize + 1);
+    canvas.beginPath();
+    canvas.moveTo(0, y);
+    canvas.lineTo(canvasWidth, y);
+    canvas.stroke();
+
+    [...Array(totalXRows).keys()].slice(1).forEach(function (xId) {
+      let x = xId * (tileSize + 1);
+      canvas.beginPath();
+      canvas.moveTo(x, 0);
+      canvas.lineTo(x, canvasHeight);
+      canvas.stroke();
+
+      let sign = asciiScene[yId - 1] && asciiScene[yId - 1][xId - 1] ? asciiScene[yId - 1][xId - 1] : ' ';
+      let elementSprite = document.getElementById('sprite-' + sign) || backgroundSpriteElement;
+      canvas.drawImage(backgroundSpriteElement, x - tileSize - 1, y - tileSize - 1, tileSize, tileSize);
+      canvas.drawImage(elementSprite, x - tileSize - 1, y - tileSize - 1, tileSize, tileSize);
+
+      asciiScene[yId - 1][xId - 1] = sign;
+    });
+  });
+
+  canvasElement.addEventListener('click', function (event) {
+    let x = event.clientX - canvasElement.offsetLeft + document.body.scrollLeft + document.documentElement.scrollLeft + canvasContainerElement.scrollLeft;
+    let y = event.clientY - canvasElement.offsetTop + document.body.scrollTop + document.documentElement.scrollTop + canvasContainerElement.scrollTop;
+
+    x = x - (x % (tileSize + 1));
+    y = y - (y % (tileSize + 1));
+
+    canvas.drawImage(backgroundSpriteElement, x, y, tileSize, tileSize);
+    canvas.drawImage(activeSpriteElement, x, y, tileSize, tileSize);
+
+    let yId = Math.floor(y / (tileSize + 1));
+    let xId = Math.floor(x / (tileSize + 1));
+
+    asciiScene[yId][xId] = activeSpriteElement.dataset.sign;
+  });
+}
+
+showProperSubMode();
+fullRedraw();
+
+document.getElementById('easy-to-ascii').addEventListener('click', function () {
+  let scene = asciiScene.map(row => {
+    return row.join('').trimRight();
+  }).join("\n");
+
+  document.getElementById('easy-textarea-level').value = scene;
+
+  subMode = 'ascii';
+  showProperSubMode();
+});
+
+document.getElementById('easy-to-image').addEventListener('click', function () {
+  let sceneString = document.getElementById('easy-textarea-level').value;
+  let scene = sceneStringToMap(sceneString);
+
+  asciiScene = combineAsciiScenes(emptyAsciiScene, scene);
+  fullRedraw();
+
+  subMode = 'image';
+  showProperSubMode();
+});
 
 function runElm() {
   document.getElementById('editor-container').style.display = 'none';
@@ -98,10 +239,7 @@ document.getElementById('advanced-textarea-level').value =
   localStorage.getItem(advancedCacheKey) || JSON.stringify(defaultLevel, null, 2);
 
 document.getElementById('easy-textarea-level').value =
-  localStorage.getItem(easyCacheKey) ||
-`..o......
-.p...**..
-.........`;
+  localStorage.getItem(easyCacheKey) || defaultEasyScene;
 
 Object.keys(levels).forEach(function(id) {
   document.getElementById(id)
